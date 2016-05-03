ccca3e16285029319060bdf955297dc74291250b
elan33 <elan0622@gmail.com>
2016-05-03 11:20:21 +0800

    Update and rename crdroid.mk to slim.mk

--- slim.mk
+++ slim.mk
@@ -14,10 +14,10 @@
 
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-PRODUCT_NAME := crdroid_kiwi
+PRODUCT_NAME := slim_kiwi
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
