648fb27b5722bf1ace357159199681f08c54ce6f
elan33 <elan0622@gmail.com>
2016-05-03 10:43:31 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -15,9 +15,9 @@
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_kiwi
+PRODUCT_NAME := crdroid_kiwi
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
